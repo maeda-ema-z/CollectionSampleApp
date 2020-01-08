c145df830e0fee0fd2ca7dee4a53c3b851be7bd9
maeda-ema-z <maeda-ema-z@densei.co.jp>
2020-01-08 12:08:06 +0800

    Initial Commit

---
+++ CollectionSampleTests/CollectionSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  CollectionSampleTests.swift
+//  CollectionSampleTests
+//
+//  Created by user164140 on 1/8/20.
+//  Copyright © 2020 user164140. All rights reserved.
+//
+
+import XCTest
+@testable import CollectionSample
+
+class CollectionSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
