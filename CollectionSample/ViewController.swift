7727ffdc4d6ae1f3ed6bb37ae077c95ccaac2ece
maeda-ema-z <maeda-ema-z@densei.co.jp>
2020-01-08 15:12:47 +0800

    add image colletcion

--- CollectionSample/ViewController.swift
+++ CollectionSample/ViewController.swift
@@ -14,7 +14,41 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController: UICollectionViewDataSource {
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: CustomCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CustomCell
+        if indexPath.row == 0 {
+            cell.lblSample.text = "Water"
+            cell.imgSample.image = UIImage(named: "water.png")
+        } else if indexPath.row == 1 {
+            cell.lblSample.text = "Drip"
+            cell.imgSample.image = UIImage(named: "drip.png")
+        } else if indexPath.row == 2 {
+            cell.lblSample.text = "Treatment"
+            cell.imgSample.image = UIImage(named: "treatment.png")
+        } else if indexPath.row == 3 {
+            cell.lblSample.text = "Toilet"
+            cell.imgSample.image = UIImage(named: "toilet.png")
+        } else {
+            cell.lblSample.text = "???"
+            cell.imgSample.image = UIImage(named: "nurse.png")
+        }
+//        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+}
